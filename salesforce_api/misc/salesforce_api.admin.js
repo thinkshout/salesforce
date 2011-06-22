0a2478f7bdca7433f1a7a7e6d23c26c09dfe611f
Evan Donovan <evan@evandonovan.org>
2011-06-22 01:51:55 -0400

    Changed to new style for attaching Drupal.behaviors (still not actually doing the show/hide however).

--- salesforce_api/misc/salesforce_api.admin.js
+++ salesforce_api/misc/salesforce_api.admin.js
@@ -1,33 +1,33 @@
-
-Drupal.behaviors.sf_fieldmap_options = function() {
-  $.fn.sf_fieldmap_option_change = function() {
-    var key = $(this).attr('id');
-    key = key.replace(/sf-fieldmap-option-/, '');
-    $(this).sf_fieldmap_option_toggle(key);
-  }
-
-  $.fn.sf_fieldmap_option_toggle = function(fieldname) { 
-    var fielddiv = '#' + fieldname + '-extra-hidden';
-    if ($(this).val() == 'fixed'
-   || $(this).val() == 'php')  {
-      $(fielddiv).show();
-    } else  {
-      $(fielddiv).hide();
-      $(fielddiv + ' input').val('');
+Drupal.behaviors.sf_fieldmap_options = {
+  attach: function() {
+    $.fn.sf_fieldmap_option_change = function() {
+      var key = $(this).attr('id');
+      key = key.replace(/sf-fieldmap-option-/, '');
+      $(this).sf_fieldmap_option_toggle(key);
     }
-  }
+
+    $.fn.sf_fieldmap_option_toggle = function(fieldname) {
+      var fielddiv = '#' + fieldname + '-extra-hidden';
+      if ($(this).val() == 'fixed'
+       || $(this).val() == 'php')  {
+        $(fielddiv).show();
+      } else  {
+        $(fielddiv).hide();
+        $(fielddiv + ' input').val('');
+      }
+    }
   
-  // Bind the change action
-  $('.sf_fieldmap_options').bind('change', function() {
-    $(this).sf_fieldmap_option_change();
-  });
+    // Bind the change action
+    $('.sf_fieldmap_options').bind('change', function() {
+      $(this).sf_fieldmap_option_change();
+    });
 
-  // Hide hidden fields
-  $(".fieldmap-extra-text").hide();
+    // Hide hidden fields
+    $(".fieldmap-extra-text").hide();
 
-  // Show any text fields that should be shown
-  $('.sf_fieldmap_options').each(function() {
-    $(this).trigger('change');
-  });
-
+    // Show any text fields that should be shown
+    $('.sf_fieldmap_options').each(function() {
+      $(this).trigger('change');
+    });
+  }
 }
