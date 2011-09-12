c65baeaac8eb227a609df40caa95dc59e85c6d0c
Brian Vuyk <brian@brianvuyk.com>
2011-09-12 09:58:23 -0400

    Add new hook_sf_notifications_processed().

--- sf_notifications/sf_notifications.api.php
+++ sf_notifications/sf_notifications.api.php
@@ -52,5 +52,37 @@ function hook_sf_notifications_check_condition($operation, $object_record, $map)
 }
 
 /**
+ * Hook called after a notification message has been processed.
+ *
+ * This hook is invoked after a notification has been processed. You can use
+ * this hook to perform any additional actions required after a node has been
+ * created, updated, or deleted as a result of a salesforce notifications
+ * message.
+ *
+ * @param $operation
+ *  The operation being performed. Either "insert", "update", or "delete".
+ * @param $object_record
+ *  The object record the operation has been performed on. An array with the
+ *  following keys:
+ *  - oid: The drupal object id of the object we are operating on. NULL if
+ *    $operation is 'insert'.
+ *  - name: The name of the fieldmap to be used.
+ *  - drupal_entity: The entity type this object should be inserted as.
+ *  - drupal_bundle: The bundle this object should be inserted as.
+ *  - fields: The fields provided in the incoming Salesforce notification.
+ *  - operation: The operation we are performing. The same as the $operation
+ *    parameter.
+ */
+function hook_sf_notifications_processed($operation, &$object_record) {
+  switch ($operation) {
+    case "insert":
+    case "update":
+      return TRUE;
+    default:
+      return FALSE;
+  }
+}
+
+/**
  * @} End of "addtogroup hooks".
  */
