41547ebfe904dea8ca617c3ffa3c1d6c2116b517
Lev Tsypin <lev@thinkshout.com>
2012-11-13 13:15:05 -0800

    Add stubs for new hooks.

--- salesforce.api.php
+++ salesforce.api.php
@@ -7,3 +7,21 @@
  * Core hooks are typically called in all modules at once using
  * module_invoke_all().
  */
+
+/**
+ * Define fieldmap types.
+ * 
+ * TODO: Document me.
+ */
+function hook_salesforce_mapping_fieldmap_type() {
+
+}
+
+/**
+ * Alter existing fieldmap types.
+ *
+ * @param $fieldmap_type
+ */
+function hook_salesforce_mapping_fieldmap_type_alter($fieldmap_type) {
+
+}
